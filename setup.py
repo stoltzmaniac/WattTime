e8552463ad9975e39f1fe72770a76f6fe72176b4
Scott Stoltzman <scott@stoltzmanconsulting.com>
2019-10-03 13:26:23 -0600

    Add open source GNU license

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='WattTime',
-    version='0.1',
-    packages=['watttime',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="watttime",
+    version="0.0.1",
+    author="Scott Stoltzman",
+    author_email="scott@stoltzmanconsulting.com",
+    description="A package to access the WattTime API v2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stoltzmaniac/WattTime",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
