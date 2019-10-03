ed8b6d65903ef5645975e741a1ddf429f14a67af
Scott Stoltzman <scott@stoltzmanconsulting.com>
2019-10-03 13:12:06 -0600

    First run of python setup.py sdist

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='WattTime',
+    version='0.1dev',
+    packages=['watttime',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
